97e54ed5239b2b72ffd4277198066c43c35afeff
biwell <gabriel.poirier@hotmail.fr>
2024-08-30 11:45:54 -0400

    Initial Commit

---
+++ MonPremierIOS/MonPremierIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  MonPremierIOSApp.swift
+//  MonPremierIOS
+//
+//  Created by Gabriel Poirier (Étudiant) on 2024-08-30.
+//
+
+import SwiftUI
+
+@main
+struct MonPremierIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
