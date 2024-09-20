e631803703ac05a27f8bdb7b22e272a74b8d2893
biwell <94551280+biwell@users.noreply.github.com>
2024-09-20 12:36:23 -0400

    Add files via upload

---
+++ etape3/etape3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  etape3
+//
+//  Created by Gabriel Poirier (Étudiant) on 2024-09-20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ etape3/etape3UITests/etape3UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  etape3UITestsLaunchTests.swift
+//  etape3UITests
+//
+//  Created by Gabriel Poirier (Étudiant) on 2024-09-20.
+//
+
+import XCTest
+
+final class etape3UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
